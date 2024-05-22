8a79599d6ff7ea5660657f13c7785eb1617afdaa
omarfrt <iroonix5@gmail.com>
2024-05-22 01:20:49 +0100

    working but super bugged version of the dnd board

--- app/page.js
+++ app/page.js
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Dnd from "@/components/dndboard/Dnd";
 
-import Boards from "@/components/Boards";
+
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Boards />
+      <Dnd />
     </main>
   );
 }
